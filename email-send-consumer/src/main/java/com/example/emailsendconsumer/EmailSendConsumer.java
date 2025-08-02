16136b4ff86aa97f19abff944b27459caa31fb6a
asdknkwqndknqkfw <codemonkey99999@gmail.com>
2025-08-03 02:33:09 +0900

    23강 Kafka의 비동기 처리로 인한 성능 이점 느껴보기

--- email-send-consumer/src/main/java/com/example/emailsendconsumer/EmailSendConsumer.java
+++ email-send-consumer/src/main/java/com/example/emailsendconsumer/EmailSendConsumer.java
@@ -15,6 +15,11 @@ public class EmailSendConsumer {
         EmailSendMessage emailSendMessage = EmailSendMessage.fromJson(message);
 
         // 실제 이메일 발송 로직 부분
+        try {
+            Thread.sleep(3000); // 가정: 이메일 발송 3초
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         System.out.println("이메일 발송 완료");
     }
