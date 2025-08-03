810947507d37f3b1a5f0f8e4aedc059af50a8b95
asdknkwqndknqkfw <codemonkey99999@gmail.com>
2025-08-03 21:27:33 +0900

    25강 [실습] Spring Boot로 Kafka에서 재시도조차 실패한 메시지를 따로 보관하기 (DLT, Dead Letter Topic)

--- email-send-consumer/src/main/java/com/example/emailsendconsumer/EmailSendConsumer.java
+++ email-send-consumer/src/main/java/com/example/emailsendconsumer/EmailSendConsumer.java
@@ -1,18 +1,30 @@
 package com.example.emailsendconsumer;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailSendConsumer {
     @KafkaListener(
-            topics = "email.send",
-            groupId = "email-send-group" // 컨슈머 그룹 이름
+        topics = "email.send",
+        groupId = "email-send-group" // 컨슈머 그룹 이름
+    )
+    @RetryableTopic(
+        attempts = "5", // 재시도 총 5회 (재시도 너무 많으면 시스템 부하)
+        backoff = @Backoff(delay = 1000, multiplier = 2)    // 1초 간격 재시도, 2배수씩(1초 > 2초 > 4초 ..)
     )
     public void consume(String message) {
         System.out.println("Kafka로부터 받아온 메시지: " + message);
 
         EmailSendMessage emailSendMessage = EmailSendMessage.fromJson(message);
+
+        if (emailSendMessage.getTo().equals("fail@naver.com")) {
+            String errorMessage = "잘못된 이메일 주소로 인해 발송 실패";
+            System.out.println(errorMessage);
+            throw new RuntimeException(errorMessage);
+        }
 
         // 실제 이메일 발송 로직 부분
         try {
